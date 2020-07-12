88614eae6c2b5a485ed8d98deecf737dd7f97858
Kevin Dao <daomkevin@gmail.com>
2020-07-12 13:28:09 -0700

    added code to member functions

--- main.cpp
+++ main.cpp
@@ -24,5 +24,41 @@ public:
 
 int main() {
     
+    
+    
     return 0;
+}
+
+void SavingsAccount::openAccount(int d, int c)
+{
+    dollars = d;
+    cents = c;
+}
+
+void SavingsAccount::makeDeposit(int d, int c)
+{
+    while (c >= 100) {
+        c -= 100;
+        d += 1;
+    }
+    
+    dollars += d;
+    cents += c;
+    
+}
+
+void SavingsAccount::makeWithdrawal(int d, int c)
+{
+    while (c >= 100) {
+        c -= 100;
+        d += 1;
+    }
+    
+    dollars -= d;
+    cents -= c;
+}
+
+void SavingsAccount::checkBalance()
+{
+    cout << "Dollars = " << dollars << "\tCents = " << cents;
 }
