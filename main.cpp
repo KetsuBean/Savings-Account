817c9a894db3d35bbe6e5b684309d3ae03c824b2
Kevin Dao <daomkevin@gmail.com>
2020-07-12 13:13:38 -0700

    created cpp file

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// This program emulates a savings account in which you can deposit, withdrawal, open account,
+// and check balance
+//
+//Kevin Dao
+//COURSE: CMPR 121 Section #68719
+//Week 4 Lab 13.5
+//
+
+#include <iostream>
+
+int main() {
+    
+    return 0;
+}
